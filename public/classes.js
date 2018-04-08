121fcbbbb4ed9eac8fa00b720ad73c28c2f2b4c6
ArmAlen <31158991+ArmAlen@users.noreply.github.com>
2018-04-08 01:24:49 -1200

    Update classes.js
    .

--- public/classes.js
+++ public/classes.js
@@ -1,4 +1,4 @@
-class Grass {
+/*class Grass {
     constructor(x, y, index) {
         this.x = x;
         this.y = y;
@@ -238,6 +238,6 @@ class Gishatich {
             }
         }
     }
-}
+}*/
 
 
