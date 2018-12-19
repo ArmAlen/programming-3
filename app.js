b5b5948cdc9916c567d7fefbe2f5acb93256666d
Alen Abrahamyan <31158991+ArmAlen@users.noreply.github.com>
2018-12-20 01:36:13 +0400

    Update app.js

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.get("/", function (req, res) {
     res.redirect("public");
 });
 
-server.listen(gametumo, function () {
+server.listen(process.env.PORT || 3000, function () {
     console.log("Server@ sksec ashxatel");
 });
 
